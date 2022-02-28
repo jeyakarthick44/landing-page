760b9c6a8e6fa0552fc695ecd8e907794117dc18
jeyakarthick44 <jeyakarthick408@gmail.com>
2022-02-28 10:35:06 +0530

    Completed

--- src/sections/Section3.js
+++ src/sections/Section3.js
@@ -1,8 +1,24 @@
 import React from "react";
 import { Box, Center, Container, Text } from "@chakra-ui/react";
-
+import { DragHandleIcon } from "@chakra-ui/icons";
+import awesome from "../components/awesome.jpg";
 const Section3 = () => {
-  return <div className="section3">hi</div>;
+  return (
+    <div className="section3">
+      <div className="wrapper">
+        <div className="zone">
+          <DragHandleIcon />
+          <h3>
+            Snippy is a rich coding snippets app that lets you create your own
+            code snippets, categorize them, and even sync them.
+          </h3>
+          <h6>Judith Black</h6>
+          <h6>CEO at PureInsights</h6>
+        </div>
+        <img className="image" src={awesome} />
+      </div>
+    </div>
+  );
 };
 
 export default Section3;
